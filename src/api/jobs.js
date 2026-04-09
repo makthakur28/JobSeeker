822a4658c9c9f78f329fad2dd201945be5f3331c
Mayank Thakur <mayankthakur289.mt@gmail.com>
2026-04-10 05:12:38 +0530

    mvp

--- src/api/jobs.js
+++ src/api/jobs.js
@@ -1,6 +1,6 @@
 import { Platform } from 'react-native';
 
-const API_URL = Platform.OS === 'web' ? '' : (Platform.OS === 'android' ? 'http://10.0.2.2:8000' : 'http://localhost:8000');
+const API_URL = 'https://job-scraper-api-production-2b6a.up.railway.app';
 
 export const fetchJobsApi = async (role, location, workType) => {
     try {
